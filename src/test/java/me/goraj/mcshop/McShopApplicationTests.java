b2545c2473cdeddd543549bf28ec9a190a589596
MrGoraj <32809530+MrGoraj@users.noreply.github.com>
2021-11-06 16:30:43 +0100

    Initial commit

---
+++ src/test/java/me/goraj/mcshop/McShopApplicationTests.java
@@ -0,0 +1,13 @@
+package me.goraj.mcshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
